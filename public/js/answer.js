6242b2cb4bedd765cc8b1f71fcd9b5c895d41405
echo 'hello world <ssmyth1071@gmail.com>
2017-10-26 23:10:47 -0400

    create li elements for each letter in correct array

--- public/js/answer.js
+++ public/js/answer.js
@@ -1,12 +1,73 @@
-answerArr =[
-  {fullName: 'Mike Schmidt',
-   sport: 'Baseball',
+const answerArr = [
+  {firstName: "mike",
+   lastName: 'schmidt',
+   fullName: 'mike schmidt',
+   sport: '⚾',
    uniformNum: 20,
    position: 'Third base',
-   status: 'retired',
-   }
+   status: 'Retired',
+   nickName: 'Schmidtty'
+ },
+ {firstName: "allen",
+  lastName: 'iverson',
+  fullName: 'allen iverson',
+  sport: '🏀',
+  uniformNum: 3,
+  position: 'Point Guard',
+  status: 'Retired',
+  nickName: 'The Answer'
+  },
+  {firstName: "julius",
+   lastName: 'erving',
+   fullName: 'julius erving',
+   sport: '🏀',
+   uniformNum: 6,
+   position: 'Forward',
+   status: 'Retired',
+   nickName: 'Doctor J'
+    },
+   {firstName: "joel",
+    lastName: 'embiid',
+    fullName: 'joel embiid',
+    sport: '🏀',
+    uniformNum: 21,
+    position: 'Center',
+    status: 'Active',
+    nickName: 'The Process'
+    },
+    {firstName: "jimmy",
+     lastName: 'rollins',
+     fullName: 'jimmy rollins',
+     sport: '⚾',
+     uniformNum: 11,
+     position: 'Shortstop',
+     status: 'Retired',
+     nickName: 'JRoll'
+    },
+    {firstName: "reggie",
+     lastName: 'white',
+     fullName: 'reggie white',
+     sport: '🏈',
+     uniformNum: 92,
+     position: 'Defensive End',
+     status: 'Retired',
+     nickName: 'The Minister'
+    },
+    {firstName: "eric",
+     lastName: 'lindros',
+     fullName: 'eric lindros',
+     sport: '🏒',
+     uniformNum: 88,
+     position: 'Defensive End',
+     status: 'Retired',
+     nickName: 'The Next One'
+    },
+
 ]
 
+
+
+⚾
 🏈
 🏒
 🏀
--- public/js/index.js
+++ public/js/index.js
@@ -7,14 +7,10 @@ const restartBtn = document.getElementById('restart');
 const lettersDiv = document.getElementById('letters');
 const progressDiv= document.getElementById('progress')
 const answerPanel = document.getElementById('answer-panel')
+const correctGuesses = document.getElementById('correct-guesses')
 const audio = document.querySelector('audio')
-// Hint HTML Elements - one by one for now
-
-
-
-
-
-// const alpha = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+const video = document.querySelector('video'  )
+// answer array of objexts - probably should move to a separate file
 const answerArr = [
   {firstName: "mike",
    lastName: 'schmidt',
@@ -22,7 +18,7 @@ const answerArr = [
    sport: '⚾',
    uniformNum: 20,
    position: 'Third base',
-   status: 'retired',
+   status: 'Retired',
    nickName: 'Schmidtty'
  },
  {firstName: "allen",
@@ -31,46 +27,101 @@ const answerArr = [
   sport: '🏀',
   uniformNum: 3,
   position: 'Point Guard',
-  status: 'retired',
+  status: 'Retired',
   nickName: 'The Answer'
-  }
+  },
+  {firstName: "julius",
+   lastName: 'erving',
+   fullName: 'julius erving',
+   sport: '🏀',
+   uniformNum: 6,
+   position: 'Forward',
+   status: 'Retired',
+   nickName: 'Doctor J'
+    },
+   {firstName: "joel",
+    lastName: 'embiid',
+    fullName: 'joel embiid',
+    sport: '🏀',
+    uniformNum: 21,
+    position: 'Center',
+    status: 'Active',
+    nickName: 'The Process'
+    },
+    {firstName: "jimmy",
+     lastName: 'rollins',
+     fullName: 'jimmy rollins',
+     sport: '⚾',
+     uniformNum: 11,
+     position: 'Shortstop',
+     status: 'Retired',
+     nickName: 'JRoll'
+    },
+    {firstName: "reggie",
+     lastName: 'white',
+     fullName: 'reggie white',
+     sport: '🏈',
+     uniformNum: 92,
+     position: 'Defensive End',
+     status: 'Retired',
+     nickName: 'The Minister'
+    },
+    {firstName: "eric",
+     lastName: 'lindros',
+     fullName: 'eric lindros',
+     sport: '🏒',
+     uniformNum: 88,
+     position: 'Defensive End',
+     status: 'Retired',
+     nickName: 'The Next One'
+    },
+
 ]
 // possibly add a sound for a correct guess abd missed guess
 const correctSound = './sounds/correct-answer.mp3'
 const wrongSound = './sounds/wrong-answer.mp3'
 const loseSound = './sounds/sadtrombone.mp3'
 
-/* notes Oct 26th
+/* notes Oct 27th
+- LOW Priority animation on the letters
+- solve button
+- stopping guesses on win/lose
+- add more name to answerArr
+- modal for the win lose gif
+-overall styling
+- timeout on the invalid already guessed piece
+DONE
 - logic to add correct guess to the hidden answer - MOSTLY DONE - display without commaa
 - logic for multiple instances of the same - element - look at Ruby code
-- logic to accoiunt for spaces - should just be a freebie
 - logic to compare the array of correct guess to the answer - ARRAY EQUALITY
-- OPTIONAL: WIN & LOSE screen -
-- LOW Priority animation on the letters style the answer and correct guesses
-- clearing the Hints div on restart and stopping guesses on win/lose
+- OPTIONAL: WIN & LOSE screen - got gifs need to add to modal or some other pop up
+- logic to account for spaces - should just be a freebie -
+  -style the answer and correct guesses
+  - figure out why space is removed when correct letter is guessed
 */
 
 
 // Game Data
-let triesCounter, answer, wrongGuesses, timeoutId, correctGuessesArray;
+let triesCounter, answer, wrongGuesses, timeoutId, space;
 
 // Game Logic
 function startGame() {
 
   wrongGuesses = [];
   randomArrnum = Math.floor((Math.random() * answerArr.length))
-  answer = answerArr[randomArrnum].firstName;
+  answer = answerArr[randomArrnum].fullName;
   triesCounter = 10;
-
-
   hiddenAnswer = [].concat(answer.split('')).fill('_');
-  // correctGuessesArray  = ['__ '.repeat(answerArr[randomArrnum].firstName.length) + '' + '__ '.repeat(answerArr[randomArrnum].lastName.length) ]
+  spacePosition= answer.indexOf(' ')
+  space = ' '
+  hiddenAnswer.splice(spacePosition,1,space)
   numTries.textContent = triesCounter;
   wrongGuessesSpan.textContent = '';
   resultDiv.textContent = ''
   // lettersDiv.textContent = alpha.join(' ').toUpperCase();
   progressDiv.textContent = '';
-  answerPanel.textContent = hiddenAnswer.join(' ')
+  correctGuesses.innerHTML = '<li>' +  hiddenAnswer.join('</li><li>') +'</li>'
+  // var ul =document.getElementsByTagName('ul')[0]
   clearHints()
 
 }
@@ -89,11 +140,12 @@ function clearHints () {
   hintThree.textContent = ''
   hintFour.textContent = ''
   hintFive.textContent = ''
+  video.classList.add('hidden')
 }
 
 
 function renderwrongGuesses() {
-  wrongGuessesSpan.textContent = wrongGuesses.sort().join('  ');
+  wrongGuessesSpan.innerHTML = '<li>' + wrongGuesses.sort().join('</li><li>') +'</li>';
 }
 
 function isValidGuess(guess) {
@@ -109,11 +161,8 @@ form.addEventListener('submit', function(event) {
   // clearTimeout(timeoutId);
 
   if (isValidGuess(guess)) {
-    // increment tries counter
-    triesCounter--;
-    numTries.textContent = triesCounter;
 
-    // add prev try to list
+
 
     if (triesCounter === 8) {
       hintOne.textContent = 'This person played: ' + answerArr[randomArrnum].sport
@@ -129,31 +178,43 @@ form.addEventListener('submit', function(event) {
     }
     if (triesCounter < 1) {
       resultDiv.textContent = 'You lost! the Phamous Philly Athlete was:' + answer.toUpperCase();
+      video.src = './sounds/lose.mp4'
+      video.classList.remove('hidden');
+      video.play()
       restartBtn.classList.remove('hidden');
       audio.src = loseSound;
+
     }
+
     if (answer.indexOf(guess) !== -1){
 
       audio.src = correctSound;
       audio.play()
-      /* research replace method - will need to replace the _ with guess letter
-       seems splice will work better replace is a string method*/
-      console.log(answer.indexOf(guess))
-      console.log(guess)
-      console.log(hiddenAnswer)
-      hiddenAnswer.splice(answer.indexOf(guess),1,guess)
-      console.log(hiddenAnswer)
-      answerPanel.textContent = hiddenAnswer.join(' ')
+
+
+
+      for (i=0;i<answer.length;i++) {
+        if (answer[i] === guess) {
+          hiddenAnswer.splice(i,1,guess);
+        }
+      }
+      correctGuesses.innerHTML = '<li>' +  hiddenAnswer.join('</li><li>') +'</li>'
     } else {
+      // increment tries counter only if miss
+      triesCounter--;
+      numTries.textContent = triesCounter;
       audio.src = wrongSound;
       audio.play()
       wrongGuesses.push(guess);
       renderwrongGuesses();
     }
 
-    if (hiddenAnswer === answer.split('')) {
+    if (hiddenAnswer.join('') === answer) {
       resultDiv.textContent = 'You guessed it! The answer is ' + answer;
       restartBtn.classList.remove('hidden');
+      video.src ='./sounds/win.mp4'
+      video.classList.remove('hidden')
+      video.play()
     }  else {
       // resultDiv.textContent = 'Higher!';
       // clearResultDiv();
