9b82ed9eee776aec078128aebcb09bfa48e0c632
echo 'hello world <ssmyth1071@gmail.com>
2017-12-16 09:36:55 -0500

    update triesCounter text to yellow at 2 tries more comments

--- public/js/index.js
+++ public/js/index.js
@@ -24,26 +24,31 @@ let triesCounter, answer, wrongGuesses ;
 ///Game Functions
 
 function startGame() {
+  // reset wrongGuesses
   wrongGuesses = [];
+  // select random answer from array
   randomArrNum = Math.floor((Math.random() * answerArr.length))
   answer = answerArr[randomArrNum].fullName;
-  triesCounter = 12;
   hiddenAnswer = answer.split('').map(letter => letter === ' '? ' ': '_');
+  // reset triesCounter number and styling
+  triesCounter = 12;
   numTries.textContent = triesCounter;
+  numTries.classList.remove('panic')
+  //reset wrong+correct guesses, hints and progress divs
   wrongGuessesSpan.textContent = '';
   resultDiv.textContent = ''
   correctGuesses.innerHTML = '<li>' +  hiddenAnswer.join('</li><li>') +'</li>'
   clearHints()
 
 }
-
+// remove progress msg after 2 seconds
 function clearResultDiv() {
   setTimeout(function(){
     resultDiv.textContent = '';
   }, 2000);
 }
 
-
+// clear hints called on startGame
 function clearHints () {
   hintOne.textContent = ''
   hintTwo.textContent = ''
@@ -53,15 +58,16 @@ function clearHints () {
   restartBtn.classList.add('hidden')
   form.solve.value = ''
 }
-
+// renderwrongGuesses called on each incorrect guess
 function renderwrongGuesses() {
   wrongGuessesSpan.innerHTML = '<li>' + wrongGuesses.sort().join('</li><li>') +'</li>';
 }
 
+// check validity of guess (make sure its a single letter that hasn't been guessed previously)
 function isValidGuess(guess) {
   return !!guess.match(/[A-z]/gi) && !wrongGuesses.includes(guess) && guess.length === 1 && !hiddenAnswer.includes(guess);
 }
-
+// when answer is matched bring up win modal and restartBtn
 function winGame() {
 modalMsg.textContent = 'You won! The Phamous Philly Athlete is: ' + answer.toUpperCase();
 restartBtn.classList.remove('hidden');
@@ -69,23 +75,24 @@ img.src = answerArr[randomArrNum].picPath
 modal.classList.remove('hidden');
 
 }
+// when triescounter is zero bring up lose modal and restartBtn
 function loseGame() {
 modalMsg.textContent = 'You lost! the Phamous Philly Athlete was: ' + answer.toUpperCase();
 img.src = './img/lose.jpg'
 modal.classList.remove('hidden');
 restartBtn.classList.remove('hidden');
 }
-
+// hide modal on restart
 function hideRestartModal() {
   modal.classList.add('hidden');
   startGame();
 }
-
+// reduce tries by one on a guess
 function decrementTries() {
   triesCounter--;
   numTries.textContent = triesCounter;
 }
-
+// add hint when triesCounter = 10,8 etc. loseGame at tries =0
 function hintLogic() {
   if (triesCounter === 10) {
     hintOne.textContent = 'This person played: ' + answerArr[randomArrNum].sport
@@ -101,6 +108,7 @@ function hintLogic() {
   }
   if (triesCounter === 2) {
     hintFive.textContent = 'This person\'s nickname is: ' + answerArr[randomArrNum].nickName
+    numTries.classList.add('panic')
   }
   if (triesCounter < 1) {
     loseGame();
